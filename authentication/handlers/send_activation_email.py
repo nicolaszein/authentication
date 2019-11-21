cb31fce548c9ec6030c18ccd2d7be1c8417c3e5b
Nicolas Zein <nicolas.zein@creditas.com.br>
2019-11-20 23:34:20 -0300

    Send Activation Email after user created

---
+++ authentication/handlers/send_activation_email.py
@@ -0,0 +1,24 @@
+from authentication.handlers._shared.base_handler import BaseHandler
+from authentication.services.sendgrid import Sendgrid as SendgridService
+
+
+class SendActivationEmail(BaseHandler):
+
+    def __init__(self):
+        self.__sendgrid_service = SendgridService()
+
+    def execute(self, user):
+        self.__sendgrid_service.send_message(
+            to=user.email,
+            subject='Confirme seu e-mail',
+            html_content=self.__build_html_content(user)
+        )
+
+    def __build_html_content(self, user):
+        return (
+            f'<p>Olá, <strong>{user.full_name}</strong></p>'
+            '<p>Para continuar acesse o link'
+            f' https://auth.nicolaszein.dev/auth/activate?activation_token={user.activation_token}'
+            ' e confirme seu e-mail!</p>'
+            '<p>Abraços,</p>'
+        )
---
+++ tests/handlers/test_send_activation_email.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+from authentication.models.user import User
+from authentication.handlers.send_activation_email import SendActivationEmail
+
+
+def build_html_content(user):
+    return (
+        f'<p>Olá, <strong>{user.full_name}</strong></p>'
+        '<p>Para continuar acesse o link'
+        f' https://auth.nicolaszein.dev/auth/activate?activation_token={user.activation_token}'
+        ' e confirme seu e-mail!</p>'
+        '<p>Abraços,</p>'
+    )
+
+
+@patch('authentication.handlers.send_activation_email.SendgridService')
+def test_send_activation_email(sendgrid_service_mock):
+    user = User()
+    user.generate_activation()
+
+    SendActivationEmail().execute(
+        user=user
+    )
+
+    sendgrid_service_mock().send_message.assert_called_once_with(
+        to=user.email,
+        subject='Confirme seu e-mail',
+        html_content=build_html_content(user)
+    )
