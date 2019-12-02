c9962cf1d2c0a9ead64982270277b978ac82978d
Nicolas Zein <nicolas.zein@creditas.com.br>
2019-12-02 14:36:54 -0300

    Send Reset Password email

--- authentication/handlers/create_reset_password_token.py
+++ authentication/handlers/create_reset_password_token.py
@@ -1,9 +1,13 @@
 from peewee import DoesNotExist
 from authentication.handlers._shared.base_handler import BaseHandler
+from authentication.handlers.send_reset_password_email import SendResetPasswordEmail
 from authentication.models.user import User
 
 
 class CreateResetPasswordToken(BaseHandler):
+
+    def __init__(self):
+        self.__send_reset_password_email = SendResetPasswordEmail()
 
     def execute(self, email):
         try:
@@ -13,3 +17,5 @@ class CreateResetPasswordToken(BaseHandler):
 
         user.generate_reset_password_token()
         user.save()
+
+        self.__send_reset_password_email.execute(user=user)
